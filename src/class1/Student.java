eab5afb8a83c6305bed2769514d82ccb7717c1fe
오지환 <dnakrk97@gmail.com>
2024-01-08 15:00:05 +0900

    initialize eureka-server

---
+++ src/class1/Student.java
@@ -0,0 +1,7 @@
+package class1;
+
+public class Student { // 붕어빵 틀
+    String name;
+    int age;
+    int grade;
+}
---
+++ src/pack/b/User.java
@@ -0,0 +1,7 @@
+package pack.b;
+
+public class User {
+    public User() {
+        System.out.println("패키기 pack.b 회원 생성");
+    }
+}
